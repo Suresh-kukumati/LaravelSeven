de8b39cc6a519ca7161925291c7cecc47840b4ec
kml.surya29@gmail.com <kml.surya29@gmail.com>
2021-01-02 20:41:01 +0530

    Second Commit Tested

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\User;
+class UserController extends Controller
+{
+    public function index(){
+        // $data = [
+        //     'name' => 'rajesh',
+        //     'email' => 'email@gmail.com',
+        //     'password' => 'password'
+        // ];
+        // // User::create($data);
+        // return User::all();
+    }
+}
